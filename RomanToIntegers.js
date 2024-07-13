4767f0a3784f42b5e515de5e58cf5ac8d89a9458
MohamedSaeed-dev <mohamedsas966@gmail.com>
2024-07-13 09:22:14 +0300

    enhance RomanToIntegers

--- RomanToIntegers.js
+++ RomanToIntegers.js
@@ -1,7 +1,6 @@
-function RomanToIntegers() {
+function RomanToIntegers(w) {
     let dic = new Map([['M', 1000], ['D', 500], ['C', 100], ['L', 50], ['X', 10], ['V', 5], ['I', 1]]);
     let newNum = 0;
-    let w = "I";
     
     for (let i = 0; i < w.length; i++) {
         if (i + 1 < w.length && dic.get(w[i]) < dic.get(w[i + 1])) {
@@ -12,3 +11,6 @@ function RomanToIntegers() {
     }
     console.log(newNum);
 }
+
+let roman = "III";
+RomanToIntegers(roman);
